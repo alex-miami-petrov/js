96531f798b5a7ff182fe297d5f5b913336c27ace
Miami <alex.miami.petrov@gmail.com>
2024-08-14 15:58:40 +0300

    m

---
+++ vite-project/menu.js
@@ -0,0 +1,10 @@
+const menuBtn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
+menuBtn.addEventListener("click", function () {
+  if (menu.style.display === "none" || menu.style.display === "") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+});
